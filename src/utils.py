cefbb09f9aaa7d3e7ae433c0d231658f77486e36
kammarishivashankar <kammarishivashankarr@gmail.com>
2025-10-05 19:05:18 +0530

    EDA commit

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,40 @@
-import sys
+from pandas import DataFrame
+import pandas as pd
+from IPython.display import display
 
-def error_message_details(error,error_detail: sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    error_message = f"Error occured in python script name [{file_name}] line number [{line_number}] error message {str(error)}"
+class EDA:
+    def __init__(self,df):
+        self.df = df
+
+    def data_overview(self,df:DataFrame):
+        """
+        returns the overview of dataset like 
+        1. number of rows and columns
+        2. Column datatypes
+        3. number of rows with missing values
+        4. 
+        """
+        print(f'shape of the dataset --> rows : {df.shape[0]} and columns : {df.shape[1]}\n')
+        print(f"Column types:\n{df.dtypes}\n")
+        print(f"number of rows with missing values :\n{df.isnull().sum()}\n")
+
+        numeric_columns = df.select_dtypes(exclude='object').columns
+        categorical_columns = df.select_dtypes(include='object').columns
+
+        resulting_df = {}
+        for column in categorical_columns:
+            resulting_df[column] = pd.Series(df[column].unique())
+        
+        final_df = pd.DataFrame(resulting_df)
+        final_df = final_df.fillna('')
+        print(f"dataframe with catgorical columns and its unique values: ")
+        display(final_df)
+
+
+
+
+
+
+        
+
     
-    return error_message
-
-
-class CustomerException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_details(error_message,error_detail = error_detail)
-
-    def __str__(self):
-        return self.error_message
-    
-
-excp_obj = CustomerException(Exception)
-
-try:
-    print(10/0)
-except Exception as e:
-    print(e)
-
-
-
-
-
