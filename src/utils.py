fd9a56a279890787259a8257752c512ff913c5cd
kammarishivashankar <kammarishivashankarr@gmail.com>
2025-10-05 17:56:41 +0530

    exception & logger commit

---
+++ src/utils.py
@@ -0,0 +1,31 @@
+import sys
+
+def error_message_details(error,error_detail: sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occured in python script name [{file_name}] line number [{line_number}] error message {str(error)}"
+    
+    return error_message
+
+
+class CustomerException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_detail = error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+excp_obj = CustomerException(Exception)
+
+try:
+    print(10/0)
+except Exception as e:
+    print(e)
+
+
+
+
+
